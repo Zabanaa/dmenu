83b083b8e30d9fb7ecb1ac6734cc17945fda1962
Zabana <karim.cheurfi@gmail.com>
2020-06-26 17:54:33 +0200

    added custom colourscheme

--- config.h
+++ config.h
@@ -11,17 +11,25 @@ static const char *fonts[] = {
 	"monospace:pixelsize=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* dark blue - #010618*/
+/* light pink - #EE7ED4 */
+/* white - #FFFFFF */
+/* whitepink - #F2E4F3 */
+/* salmon - #EE7C74 */
+
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#F2E4F3", "#010618" },
+	[SchemeSel] = { "#FFFFFF", "#EE7ED4" },
+	[SchemeSelHighlight] = {"#010618", "#42Aff8" },
+	[SchemeNormHighlight] = {"#010618", "#42Aff8" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+static unsigned int lines      = 8;
 static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 
 /*
